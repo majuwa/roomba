17aa88fdd32afd6295524bd14fb1cef4892248c0
Stefan Bodenmüller <bodenmst@sik-n3027-157.informatik.uni-augsburg.de>
2015-06-26 16:21:49 +0200

    Music

---
+++ lib/music.c
@@ -0,0 +1,76 @@
+#include <music.h>
+
+void save_songs() {
+	int song0 [] = {A4, Q, A4, Q, A4, Q, F4, ED, C5, S, A4, Q, F4, ED, C5, S, A4, HALF};
+	int song1 [] = {E5, Q, E5, Q, E5, Q, F5, ED, C5, S, GIS4, Q, F4, ED, C5, S, A4, HALF};
+	int song2 [] = {A5, Q, A4, ED, A4, S, A5, Q, GIS5, E, G5, E, FIS5, S, F5, S, FIS5, S};
+	int song3 [] = {R, E, AIS4, E, DIS5, Q, D5, E, CIS5, E, C5, S, B4, S, C5, E};
+	int song4 [] = {R, E, F4, E, GIS4, Q, F4, ED, GIS4, S, C5, Q, A4, ED, C5, S, E5, HALF};
+	int song5 [] = {R, E, F4, E, GIS4, Q, F4, ED, C5, S};
+	
+	send_byte_roomba(140);
+	send_byte_roomba(0);
+	send_byte_roomba(9);
+	for(int i = 0; i < 18; i++) {
+		send_byte_roomba(song0[i]);
+	}
+	
+	send_byte_roomba(140);
+	send_byte_roomba(1);
+	send_byte_roomba(9);
+	for(int i = 0; i < 18; i++) {
+		send_byte_roomba(song1[i]);
+	}
+	
+	send_byte_roomba(140);
+	send_byte_roomba(2);
+	send_byte_roomba(9);
+	for(int i = 0; i < 18; i++) {
+		send_byte_roomba(song2[i]);
+	}
+	
+	send_byte_roomba(140);
+	send_byte_roomba(3);
+	send_byte_roomba(8);
+	for(int i = 0; i < 16; i++) {
+		send_byte_roomba(song3[i]);
+	}
+	
+	send_byte_roomba(140);
+	send_byte_roomba(4);
+	send_byte_roomba(9);
+	for(int i = 0; i < 19; i++) {
+		send_byte_roomba(song4[i]);
+	}
+}
+
+void play_songs() {
+	send_byte_roomba(141);
+	send_byte_roomba(0);
+	my_msleep(MEASURE_TIME*3000);
+	
+	send_byte_roomba(141);
+	send_byte_roomba(1);
+	my_msleep(MEASURE_TIME*3000);
+	
+	send_byte_roomba(141);
+	send_byte_roomba(2);
+	my_msleep(MEASURE_TIME*2500);
+	
+	send_byte_roomba(141);
+	send_byte_roomba(3);
+	my_msleep(MEASURE_TIME*1500);
+	
+	send_byte_roomba(141);
+	send_byte_roomba(4);
+	my_msleep(MEASURE_TIME*2500);
+	
+	send_byte_roomba(141);
+	send_byte_roomba(2);
+	my_msleep(MEASURE_TIME*2500);
+	
+	send_byte_roomba(141);
+	send_byte_roomba(3);
+	my_msleep(MEASURE_TIME*2000);
+}
+
---
+++ lib/music.h
@@ -0,0 +1,55 @@
+#ifndef MUSIC_H_INCLUDED
+#define MUSIC_H_INCLUDED
+
+#include <usart.h>
+#include <avr/io.h>
+#include <tools.h>
+
+#define R 30
+
+#define C4 60
+#define CIS4 61
+#define D4 62
+#define DIS4 63
+#define E4 64
+#define F4 65
+#define FIS4 66
+#define G4 67
+#define GIS4 68
+#define A4 69
+#define AIS4 70
+#define B4 71
+#define C5 72
+#define CIS5 73
+#define D5 74
+#define DIS5 75
+#define E5 76
+#define F5 77
+#define FIS5 78
+#define G5 79
+#define GIS5 80
+#define A5 81
+#define AIS5 82
+#define B5 83
+#define C6 84
+#define CIS6 85
+#define D6 86
+#define DIS6 87
+#define E6 88
+#define F6 89
+#define FIS6 90
+
+#define MEASURE 160
+#define HALF MEASURE/2
+#define Q MEASURE/4
+#define E MEASURE/8
+#define ED MEASURE*3/16
+#define S MEASURE/16
+ 
+#define MEASURE_TIME MEASURE/64
+
+void save_songs(void);
+
+void play_songs(void);
+
+#endif
